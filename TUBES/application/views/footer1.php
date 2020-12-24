7bcfcdad50b3e86dce1379d71b7859daae8545db
arsinta1405 <76531185+arsinta1405@users.noreply.github.com>
2020-12-25 01:09:37 +0700

    sudah dicocokin sama cssnya

---
+++ TUBES/application/views/footer1.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/stylefooter.css')?>">
+        <link href="//db.onlinewebfonts.com/c/103581eb752b4fa618534d266d46d568?family=HelveticaNeueLT+Pro+55+Roman" rel="stylesheet" type="text/css"/>
+        <link href="//db.onlinewebfonts.com/c/5fe894d3e06d8f07fbe9d80b3157afd5?family=HelveticaNeueLT+Pro+55+Roman" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <footer id="site-footer" class="">
+            <div class="upper-footer container-fluid p-4">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="footer-nav-left mb-4">
+                            <ul class="list-unstyled d-flex">
+                                <li class="px-3"><a href="/products" data-value="Products" data-type="CTALink" data-section="Footer">Products</a></li>
+                                <li class="px-3"><a href="/solutions" data-value="Solutions" data-type="CTALink" data-section="Footer">Solutions</a></li>
+                                <li class="px-3"><a href="/support" data-value="Support" data-type="CTALink" data-section="Footer">Support</a></li>
+                                <li class="px-3"><a href="/insight" data-value="Insight" data-type="CTALink" data-section="Footer">Insight</a></li>
+                                <li class="px-3"><a href="/about" data-value="About" data-type="CTALink" data-section="Footer">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="footer-logo"><img src=<?= base_url('assets/photo/')."footer-logo.png"?> alt="Micron blue M encircled with slanted blue line " class="img-fluid pb-4" style="min-width: 150px; height: auto; min-height: 150px; max-height: 150px; width: auto;"></div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="footer-nav-right">
+                            <div class="footer-buttons d-flex justify-content-end mb-4">
+                                <a href="/support/sales-network" data-value="Sales" data-type="CTALink" data-section="Footer" class="btn px-4 mx-2" style="color:gray; font-size: 14px">Sales</a>
+                                <a href="/support" data-value="Support" data-type="CTALink" data-section="Footer" class="btn px-4 mx-2"style="color:gray; font-size:14px">Support</a>
+                                <a href="/forms/contact-us" data-value="Contact" data-type="CTALink" data-section="Footer" class="btn px-4 mx-2"style="color:gray; font-size:14px">Contact</a>
+                            </div>
+                        </div>
+                        <div class="additional-logos d-flex justify-content-end">
+                            <a href="https://www.crucial.com/" data-type="CTALink" data-value="Crucial logo" data-section="Footer" class=""><img src=<?= base_url('assets/photo/').'crucial.png'?> alt="Crucial by Micron"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="lower-footer container-fluid">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <ul class="list-unstyled d-flex" >
+                            <li class="p-4"><a href="/legal" data-value="Legal" data-type="CTALink" data-section="Footer" class="text-dark">Legal</a></li>
+                            <li class="p-4"><a href="/privacy" data-value="Privacy" data-type="CTALink" data-section="Footer" class="text-dark">Privacy</a></li>
+                            <li class="p-4"><a href="/about/our-commitment/sourcing-responsibly/suppliers" data-value="Suppliers" data-type="CTALink" data-section="Footer" class="text-dark">Suppliers</a></li>
+                            <li class="p-4"><a href="/careers" data-value="Careers" data-type="CTALink" data-section="Footer" class="text-dark">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="copyright-message ">©2020 Micron Technology, Inc. All rights reserved. Information, products, and/or specifications are subject to change without notice. All information is provided on an “AS IS” basis without warranties of any kind. Drawings may not be to scale. Micron, the Micron logo, and all other Micron trademarks are the property of Micron Technology, Inc. All other trademarks are the property of their respective owners.</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ TUBES/application/views/landing1.php
@@ -0,0 +1,137 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/stylelanding.css')?>">
+        <link href="//db.onlinewebfonts.com/c/103581eb752b4fa618534d266d46d568?family=HelveticaNeueLT+Pro+55+Roman" rel="stylesheet" type="text/css"/>
+        <link href="//db.onlinewebfonts.com/c/5fe894d3e06d8f07fbe9d80b3157afd5?family=HelveticaNeueLT+Pro+55+Roman" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <!-- tinggal rapihin sama kasih gambar tiap jumbotron sama card-deck yang pertama -->
+        <section class="container-fluid" id="jumbotron1">
+            <div class="row justify-content" style="background:url('assets/photo/headerlanding.jpg');background-position: left; background-size:105%; height: 410px; margin-top: 75px">
+                <div class="jumbotron-fluid bg-transparent" style="height:1000px;">
+                    <h1 class="display-4">Unique Suggestions</h1>
+                    <p class="lead text-light">Micron's memory and storage innovations are helping you shop, binge and surf in a more personalized way.</p>
+                    <p class="lead">
+                        <a class="btn btn-light" href="#" role="button" style="color: #0090DA; font-text: 18px">Read more</a>
+                    </p>
+                </div>
+            </div>
+        </section>        
+        <section class="container-fluid">
+            <div class="card-deck">
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body ">
+                            <a href="#">
+                                <img class="card-img-top" src=<?= base_url('assets/photo/micronforall.jpg')?> alt="gambar 1">
+                                <h5 style="font-size:24px">Micron For All</h5>
+                            </a>
+                            <p class="card-text">Micron’s 2020 DEI report measures the company’s progress in creating a positive, 
+                                inclusive culture and our commitment to serve our communities and welcome the next generation of engineers 
+                                and inventors.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body">
+                            <a href="#">
+                                <img class="card-img-top" src=<?= base_url('assets/photo/176nand.jpg')?> alt="gambar 2">
+                                <h5 style="font-size:24px">Meet the World’s First 176-Layer NAND</h5>
+                            </a>
+                            <p class="card-text">Micron ships industry’s densest, most technologically advanced NAND.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body ">
+                            <a href="#">
+                                <img class="card-img-top" src=<?= base_url('assets/photo/ethernet.jpg')?> alt="gambar 3">
+                                <h5 style="font-size:24px">Ethernet Bunch of Flash in an NVMe-oF™ Network for Low-Cost Storage at Scale</h5>
+                            </a>
+                            <p class="card-text">Micron is helping to unlock the benefits of NVMe-oF by collaborating with key 
+                                technology partners and innovating new technologies.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body ">
+                            <a href="#">
+                                <img class="card-img-top" src=<?= base_url('assets/photo/covid.jpg')?> alt="gambar 4">
+                                <h5 style="font-size:24px">Supporting our Community</h5>
+                            </a>
+                            <p class="card-text">Learn how we’re supporting our team members, business, and communities during 
+                                these rapidly evolving times.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container-fluid" id="jumbotron2">
+            <div class="row justify-content-center">
+                <div class="jumbotron-fluid" style="width: 100%; max-width: 670px; height: 450px">
+                    <h2 class="display-4">The future starts here</h2>
+                    <p class="lead text-dark">We define our future together. Micron innovation and your inspiration are shaping
+                         tomorrow's products, industries, and the world.</p>
+                </div>
+            </div>
+        </section>
+        <section class="container-fluid" id="jumbotron3">
+            <div class="row justify-content-left"  style="background:url('assets/photo/team.jpg'); height : 465px; background-size: 100%; margin-top : 180px">
+                <div class="jumbotron-fluid" style="max-height: 550px">
+                    <h3 class="display-4">Change the world. Join our team.</h3>
+                    <p class="lead">Our memory needs your mind. At Micron, we're looking for visionaries, scientists, 
+                        leaders, and believers to help us develop technologies that will transform our world. Join us.</p>
+                    <div>
+                        <br style="line-height: 1.75rem;"><br style="line-height: 1.75rem;"><br style="line-height: 1.75rem;"><br style="line-height: 1.75rem;">
+                    </div>
+                    <p class="lead">
+                        <a class="btn btn-lg" href="#" role="button">Discover careers</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="container-fluid">
+            <div class="row justify-content-center card-deck">
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body">
+                            <h4>Delivering Innovation Through Diversity</h4>
+                            <p class="card-text">We firmly believe a workforce with diverse backgrounds, 
+                                experiences, and perspectives — that is focused on inclusion — makes Micron
+                                 a better place to work.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body">
+                            <h4>Helping Students Start Careers</h4>
+                            <p class="card-text">Translate your education and talent into a career fueled by possibilities. 
+                                Micron can help you jumpstart your career and move seamlessly from education to innovation.</p>
+                            <a href="#">Find out how></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="border: none;">
+                        <div class="card-body">
+                            <h4>Supporting Educators, Students, and Our Communities</h4>
+                            <p class="card-text">We are focused on developing effective programs that promote science, technology,
+                                 engineering, and math education in the communities where Micron employees live.</p>
+                            <a href="#">Learn more ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
